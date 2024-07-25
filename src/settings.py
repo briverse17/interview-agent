dd7f714fd8726434c8bc372ff8dab0a95e0910d9
cnvs_minh_vu <nguyenminhvu@canvas-works.asia>
2024-07-25 06:27:36 +0000

    add a DE application

--- src/settings.py
+++ src/settings.py
@@ -4,22 +4,23 @@ DIRECTORIES = {
     "job": "data/jobs/",
     "candidate": "data/candidates/",
     "instruction": "data/model_instructions/",
+    "cache": "data/caches",
+    "report": "data/reports",
 }
 
 APPLICATIONS = {
     "aie-nmv": {
         "job": {"id": "aie", "filename": "aie.md"},
         "candidate": {"id": "nmv", "filename": "nmv.md"},
-    }
+    },
+    "de-nmv": {
+        "job": {"id": "de", "filename": "de.txt"},
+        "candidate": {"id": "nmv", "filename": "nmv.md"},
+    },
 }
 
 APIKEY = st.secrets["GEMINI_API_KEY"]
 MODEL_NAME = "gemini-1.5-flash"
-MODEL_INSTRUCTIONS = {
-    "system": "system.md",
-    "read_jd": "read_jd.md",
-    "start": "start.md",
-}
 
 DEBUGGING = bool(st.secrets["DEBUGGING"])
 print("Debugging:", DEBUGGING)
